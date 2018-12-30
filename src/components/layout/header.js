87397bab76c7cc93f105bcc81d3016d86b94dac7
Alexandra Moore <alexmoore94@hotmail.co.uk>
2018-12-30 15:15:28 +0000

    new image added and css modules used

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -2,32 +2,15 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import headerStyles from './css-modules/header.module.css'
+
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <div className={headerStyles.container}>
+    <h1 className={headerStyles.heading}>
+      <Link to="/">
+        {siteTitle}
+      </Link>
+    </h1>
   </div>
 )
 
