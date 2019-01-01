e5b7647c358cc9ba7c08f5324af118952ca64fa7
Alexandra Moore <alexmoore94@hotmail.co.uk>
2019-01-01 17:26:46 +0000

    minor formatting and layout added to blog post page

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,20 +1,24 @@
-import React from "react";
-import Helmet from "react-helmet";
+import React from 'react'
+import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
+
+import Layout from '../components/layout/layout'
 
 export default function Template({data}) {
   const post = data.markdownRemark;
   return (
-    <div className="blog-post-container">
-     <Helmet title={`CodeStack - ${post.frontmatter.title}`} />
-      <div className="blog-post">
-        <h1>{post.frontmatter.title}</h1>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: post.html }}
-        />
+    <Layout>
+      <div className="blog-post-container">
+       <Helmet title={`${post.frontmatter.title}`} />
+        <div className="blog-post">
+          <h1>{post.frontmatter.title}</h1>
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          />
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
 
