b1e18fca30299a1e0d915e777617b1c4f9e2e0ce
Alexandra Moore <alexmoore94@hotmail.co.uk>
2019-05-05 10:58:25 +0100

    mystery style removed, hot-reload package re-added

--- gatsby-node.js
+++ gatsby-node.js
@@ -35,3 +35,13 @@ result.data.allMarkdownRemark.edges
         });
     });
 }
+
+exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
+  const config = getConfig()
+  if (stage.startsWith('develop') && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react-dom': '@hot-loader/react-dom'
+    }
+  }
+}
