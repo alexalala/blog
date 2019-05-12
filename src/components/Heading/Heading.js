a42cef5cd95fe3743e74f1a2d52252f160617d64
Alexandra Moore <alexmoore94@hotmail.co.uk>
2019-05-12 17:45:38 +0100

    img added to the correct page

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react'
 
 import headingStyles from './heading.module.scss'
-import me from '../../assets/img/me.jpg'
 
 export default function Heading({title}){
   return (
@@ -9,7 +8,6 @@ export default function Heading({title}){
       <div className={headingStyles.container}>
         <h1>{title}</h1>
       </div>
-      <img className={headingStyles.img} src={me} alt="Alexandra Moore" />
     </Fragment>
   );
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 import Layout from '../components/layout/layout'
 import Heading from '../components/Heading/Heading.js'
+import me from '../assets/img/me.jpg'
+
+import aboutStyles from './about.module.scss'
 
 export default function About(){
   return (
     <Layout>
       <Heading title="About" />
+      <img className={aboutStyles.img} src={me} alt="Alexandra Moore" />
     </Layout>
   );
 }
