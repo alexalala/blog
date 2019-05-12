aa5619007cda7530bd6e8e36c499ff88dc149911
Alexandra Moore <alexmoore94@hotmail.co.uk>
2019-05-12 17:20:06 +0100

    image added into about page and styled

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import headingStyles from './heading.module.scss'
+import me from '../../assets/img/me.jpg'
 
 export default function Heading({title}){
   return (
-    <div className={headingStyles.container}>
-      <h1>{title}</h1>
-    </div>
+    <Fragment>
+      <div className={headingStyles.container}>
+        <h1>{title}</h1>
+      </div>
+      <img className={headingStyles.img} src={me} alt="Alexandra Moore" />
+    </Fragment>
   );
 }
