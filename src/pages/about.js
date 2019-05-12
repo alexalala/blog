fce0a052185edd130f7d3a8c810915876e8f5ffa
Alexandra Moore <alexmoore94@hotmail.co.uk>
2019-05-12 21:55:08 +0100

    text added to about page and styled

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,6 +11,11 @@ export default function About(){
     <Layout>
       <Heading title="About" />
       <img className={aboutStyles.img} src={me} alt="Alexandra Moore" />
+      <div>
+        <p>I’m Alexandra, a self-taught Front End Developer.</p>
+        <p>I’ve been working as a developer since ’17 & teaching myself to code since ’14.</p>
+        <p>I am passionate about inclusivity - both through my code in making sure it’s always accessible, as well as through helping others from a range of backgrounds get into the industry.</p>
+      </div>
     </Layout>
   );
 }
